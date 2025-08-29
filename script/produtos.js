8b7758a4f8bbe55340466bbe6708221e6213f3a0
Gustavooli122 <gugugo959888@gmail.com>
2025-08-29 16:06:00 -0300

    Update produtos.js

--- script/produtos.js
+++ script/produtos.js
@@ -92,11 +92,11 @@ produtos.forEach(produto =>{
       <img src="${produto.imagem}" alt="img-produto-card" 
            class="w-full object-cover rounded-t-md">
     </a>
-    <div class="flex flex-col lg:justify-between md:justify-between sm:justify-between lg:flex-row md:flex-row sm:flex-row items-center mx-2 mt-2">
+    <div class="flex flex-col lg:justify-between md:justify-between sm:justify-between lg:flex-row md:flex-row sm:flex-row lg:items-center md:items-center sm:items-center items-start  mx-2 mt-2">
       <ul id = "rating" class="flex gap-1">
     ${gerarEstrelas(produto.rating)}
       </ul>
-      <p class="text-gray-400  lg:text-xs md:text-xs sm:text-xs text-sm mt-1">R$ ${produto.preco.toFixed(2)}</p>
+      <p class="text-gray-400  lg:text-xs md:text-xs sm:text-xs text-sm">R$ ${produto.preco.toFixed(2)}</p>
     </div>
     <div class="mx-2 mt-2">
       <h2 class="text-sm font-medium leading-tight">${produto.descricao}</h2>
@@ -114,16 +114,13 @@ function gerarEstrelas(rating) {
   let estrelas = "";
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
-      estrelas += `<i class="fa fa-star text-yellow-500  lg:text-xs md:text-xs sm:text-xs text-sm"></i>`;
+      estrelas += `<i class="fa fa-star text-yellow-500  lg:text-xs md:text-xs sm:text-xs text-sm my-2"></i>`;
     } else {
-      estrelas += `<i class="fa fa-star text-gray-300 lg:text-xs md:text-xs sm:text-xs text-sm"></i>`;
+      estrelas += `<i class="fa fa-star text-gray-300 lg:text-xs md:text-xs sm:text-xs text-sm my-2"></i>`;
     }
   }
   return estrelas;
 }
-
-
-
 
 
 
