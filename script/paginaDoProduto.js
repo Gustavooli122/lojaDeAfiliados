d4f4b613b9a0c75b557625207416cf42aeba25d0
Gustavooli122 <gugugo959888@gmail.com>
2025-09-08 17:37:20 -0300

    Update paginaDoProduto.js

--- script/paginaDoProduto.js
+++ script/paginaDoProduto.js
@@ -54,6 +54,8 @@ const slides = document.getElementById('slides');
     const produtoAtual = produtos.find((p)=>p.id === produtoId)
 
     const relacionados = produtos.filter((p)=>p.categoria === produtoAtual.categoria && p.id !== produtoAtual.id);
+    const produtoCompleto = [produtoAtual]; // transforma em array para poder usar forEach
+
 
     const carrossel = document.querySelector(".cardCarrossel");
     
@@ -71,16 +73,19 @@ const slides = document.getElementById('slides');
     </div>
     <div class="mx-2 mt-2">
       <h2 class="text-sm font-medium leading-tight">${produto.nome}</h2>
-      <button class="bg-[#0d2f42] text-gray-200 text-xs px-2 py-1 mt-2 rounded hover:bg-[#123f59] w-full">
-        <i class="fa-solid fa-cart-shopping text-white mr-1"></i>
-        Carrinho
+      <a href="produto.html?id=${produto.id}">
+     <button class="bg-[#0d2f42] text-gray-200 text-xs px-2 py-1 mt-2 rounded hover:bg-[#123f59] w-full">
+        Ver mais
       </button>
+    </a> 
     </div>
   </article>`;
 
+  
   carrossel.innerHTML += card;
     })
 
+  
       
   let isDown = false;    // verifica se está clicado
 let comecoX;            // posição inicial do mouse/toque
@@ -124,3 +129,92 @@ carrossel.addEventListener("touchmove", (e) => {
   const walk = (x - comecoX) * 2;
   carrossel.scrollLeft = scrollLeft - walk;
 });
+
+
+const cardDeInformacoes = document.querySelector(".cardDeinformacoes");
+    
+    produtoCompleto.forEach(produto =>{
+       const card2 = `<header>
+    <h1 class="text-3xl sm:text-4xl font-[300]">${produto.nome}</h1> 
+   
+    <p class="preco text-xl sm:text-2xl text-gray-700 my-3" aria-label="Preço do produto">$ 25,00</p>
+  </header>
+
+  <!-- Avaliação -->
+   <section aria-label="Avaliação do produto" class="flex flex-col sm:flex-row md:flex-row lg:flex-row gap-3 sm:gap-5 my-3 sm:my-4 sm:items-center">
+    <ul class="flex gap-2 md:flex-wrap md:w-[60%] sm:gap-3" role="list" aria-label="Estrelas da avaliação">
+      <li><i class="fa fa-star text-base" style="color: #facc15;" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star text-base" style="color: #facc15;" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star text-base" style="color: #facc15;" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star text-base" style="color: #facc15;" aria-hidden="true"></i></li>
+      <li><i class="fa fa-star text-base" style="color: #979797;" aria-hidden="true"></i></li>
+    </ul>
+    <p class="text-lg">Avaliação <strong>4,8</strong> | 36 comentários</p>
+  </section>
+
+  <!-- Detalhes do produto -->
+  <dl class="font-medium flex gap-2 my-3">
+    <dt><strong>Marca:</strong></dt>
+    <dd class="text-gray-500">Fácil de usar</dd>
+  </dl>
+
+  <!-- Descrição -->
+  <section class="my-3">
+    <h2><strong>Descrição:</strong></h2>
+    <p class="text-lg">
+      ${produto.descricao}
+    </p>
+  </section>
+
+  <!-- Cor -->
+  <section class="flex gap-2 my-3">
+    <h2><strong>Cor  disponível:</strong></h2>
+    <p class="text-gray-500">Branco / Preto</p>
+  </section>
+
+  <!-- Especificações -->
+  <section class="flex flex-col gap-2 my-3">
+    <h2><strong>Especificações:</strong> </h2>
+    <p class="text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+      Conjunto você está irritado.  
+      Ut enim ad minim, dolore magna aliqua.  
+      Excepto sint.  
+    </p>
+  </section>
+
+<section class="flex gap-2 sm:gap-2 flex-col sm:flex-row md:flex-row lg:flex-row my-3 sm:justify-between w-full sm:px-0">
+  <a href="#" aria-label="Link do produto" 
+     class="bg-[#0b2c57d5] hover:bg-[#238a48] text-white py-2 px-3 rounded-[4px] text-xl w-auto text-center sm:w-full">
+     <button class="p-0 m-0">Comprar agora</button>
+  </a>
+  <button id="btnFavoritos" class="bg-[#2ea55cd5] hover:bg-[#238a48] text-white py-2 px-3 rounded-[4px] text-[18px] w-auto sm:w-full">
+  
+<i class="fa-regular fa-heart mx-2 sm:mx-0 iconeFavoritoProduto"></i> Adicionar aos favoritos
+  </button>
+
+  <script>
+  // Aguarda o HTML carregar antes de rodar
+  document.addEventListener("DOMContentLoaded", () => {
+    const btnFavoritos = document.getElementById("btnFavoritos");
+    const iconeFavoritosContorno = document.querySelector(".iconeFavoritoProduto");
+
+    btnFavoritos.addEventListener("click", () => {
+      if(document.querySelector(".fa-regular")){ 
+      iconeFavoritosContorno.classList.remove("fa-regular");
+      iconeFavoritosContorno.classList.add("fa-solid");}
+      else{
+      iconeFavoritosContorno.classList.add("fa-regular");
+      iconeFavoritosContorno.classList.remove("fa-solid");
+      }
+    });
+  });
+</script>
+</section>`;
+
+  
+ cardDeInformacoes.innerHTML += card2;
+    })
+  
+  
+
