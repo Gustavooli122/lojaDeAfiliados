b063fdef3bc1bf0d8cb5333022bd7c98f37bb39f
Gustavooli122 <gugugo959888@gmail.com>
2025-09-17 18:08:53 -0300

    Update produtos.js

--- script/produtos.js
+++ script/produtos.js
@@ -3,10 +3,10 @@
 const produtos = [
   {
     id: 1,
-    nome: "Produto Teste",
+    nome: "Produto Teste categoria A",
     descricao: "Produto resistente para treinos intensos.",
     preco:950.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "https://static.netshoes.com.br/produtos/bicicleta-aro-29-krw-aluminio-24-vel-marchas-freio-a-disco-suspensao-dianteira-mountain-bike-x32/08/CGY-0302-108/CGY-0302-108_zoom1.jpg?ts=1745398163&ims=1088x",
      outrasImg: ["../img/feature_prod_02.jpg","../img/feature_prod_03.jpg","../img/feature_prod_01.jpg","../img/feature_prod_03.jpg","../img/feature_prod_02.jpg"],
     rating: 4, // até 5
     categoria:"A"
@@ -16,7 +16,7 @@ const produtos = [
     nome: "Peso de academia categoriaC",
     descricao: "O produto de CategoriaC id 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse. Donec condimentum elementum convallis. Nunc sed orci a diam ultrices aliquet interdum quis nulla.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "https://images.tcdn.com.br/img/img_prod/394779/bicicleta_29_gts_m1_freio_a_disco_21_marchas_shimano_ride_new_g_series_4993_variacao_16043_1_db4f41946fc392eb4eb4d284ddae2822_20250701155948.jpg",
     outrasImg: ["","","","","","","","",""],
     rating: 4 , // até 5
     categoria:"C"
@@ -26,24 +26,24 @@ const produtos = [
     nome: "Peso de academia categoriaA id3",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
-     outrasImg: ["","","","","","","","",""],
+    imagem: "https://http2.mlstatic.com/D_NQ_NP_922021-MLB78734496967_082024-O-bicicleta-aro-29-gta-21v-freio-disco-mecnico-suspenso-bike.webp",
+     outrasImg: ["https://http2.mlstatic.com/D_NQ_NP_922021-MLB78734496967_082024-O-bicicleta-aro-29-gta-21v-freio-disco-mecnico-suspenso-bike.webp","https://http2.mlstatic.com/D_NQ_NP_922021-MLB78734496967_082024-O-bicicleta-aro-29-gta-21v-freio-disco-mecnico-suspenso-bike.webp","https://http2.mlstatic.com/D_NQ_NP_922021-MLB78734496967_082024-O-bicicleta-aro-29-gta-21v-freio-disco-mecnico-suspenso-bike.webp","https://http2.mlstatic.com/D_NQ_NP_922021-MLB78734496967_082024-O-bicicleta-aro-29-gta-21v-freio-disco-mecnico-suspenso-bike.webp"],
     rating: 4, // até 5
     categoria:"A"
   },
     {
     id: 4,
-    nome: "Peso de academia",
+    nome: "Peso de academia categoria B",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "../img/feature_prod_02.jpg",
      outrasImg: ["","","","","","","","",""],
     rating: 4 , // até 5
     categoria:"B"
   },
     {
     id: 5,
-    nome: "Peso de academia",
+    nome: "Peso de academia categoria B",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
     imagem: "../img/feature_prod_01.jpg",
@@ -56,7 +56,7 @@ const produtos = [
     nome: "Peso de academia categoriaA id6",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "../img/feature_prod_03.jpg",
      outrasImg: ["","","","","","","","",""],
     rating: 4 , // até 5
     categoria:"A"
@@ -66,17 +66,17 @@ const produtos = [
     nome: "Peso de academia categoriaA id7",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
-     outrasImg: ["","","","","","","","",""],
+    imagem: "../img/feature_prod_02.jpg",
+    outrasImg: ["","","","","","","","",""],
     rating: 4 , // até 5
     categoria:"A"
   },
     {
     id: 8,
-    nome: "Peso de academia",
+    nome: "Peso de academia categoria C",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "../img/feature_prod_03.jpg",
      outrasImg: ["","","","","","","","",""],
     rating: 2, // até 5
     categoria:"C"
@@ -96,28 +96,69 @@ const produtos = [
     nome: "Peso de academia categoriaA id10",
     descricao: "Produto resistente para treinos intensos.",
     preco: 240.00,
-    imagem: "../img/feature_prod_01.jpg",
+    imagem: "../img/feature_prod_02.jpg",
      outrasImg: ["","","","","","","","",""],
     rating: 5, // até 5
     categoria:"A"
+  },
+   {
+    id: 11,
+    nome: "Peso de academia categoriaD",
+    descricao: "Produto resistente para treinos intensos.",
+    preco: 240.00,
+    imagem: "../img/feature_prod_02.jpg",
+     outrasImg: ["","","","","","","","",""],
+    rating: 5, // até 5
+    categoria:"D"
+  },
+   {
+    id: 12,
+    nome: "Peso de academia categoriaD",
+    descricao: "Produto resistente para treinos intensos.",
+    preco: 240.00,
+    imagem: "../img/feature_prod_02.jpg",
+     outrasImg: ["","","","","","","","",""],
+    rating: 5, // até 5
+    categoria:"D"
+  },
+   {
+    id: 13,
+    nome: "Peso de academia categoriaD",
+    descricao: "Produto resistente para treinos intensos.",
+    preco: 240.00,
+    imagem: "../img/feature_prod_02.jpg",
+     outrasImg: ["","","","","","","","",""],
+    rating: 5, // até 5
+    categoria:"D"
+  },
+   {
+    id: 14,
+    nome: "Peso de academia categoriaC",
+    descricao: "Produto resistente para treinos intensos.",
+    preco: 240.00,
+    imagem: "../img/feature_prod_02.jpg",
+     outrasImg: ["","","","","","","","",""],
+    rating: 5, // até 5
+    categoria:"C"
   },
 
 ];
 
 const container = document.querySelector(".containerShop");
 produtos.forEach(produto =>{
-    const card = `<article class="bg-white pb-3 flex flex-col rounded-md shadow-md shadow-slate-600">
+    const card = `<article class="bg-white pb-3 flex flex-col justify-between rounded-md shadow-md shadow-slate-600">
     <a href="produto.html?id=${produto.id}">
       <img src="${produto.imagem}" alt="img-produto-card" 
-           class="w-full object-cover rounded-t-md">
+           class="w-full   rounded-t-md">
     </a>
-    <div class="flex flex-col lg:justify-between md:justify-between sm:justify-between lg:flex-row md:flex-row sm:flex-row lg:items-center md:items-center sm:items-center items-start  mx-2 mt-2">
+  
+    <div class="mx-2 mt-2"> 
+     <div class="flex flex-col  sm:justify-between lg:flex-row md:flex-col sm:flex-row lg:items-center md:items-start sm:items-center items-start lg:justify-between mt-2">
       <ul id = "rating" class="flex gap-1">
     ${gerarEstrelas(produto.rating)}
       </ul>
-      <p class="text-gray-400  lg:text-xs md:text-xs sm:text-xs text-sm">R$ ${produto.preco.toFixed(2)}</p>
+      <p class="text-gray-400  lg:text-base md:text-sm sm:text-xs text-sm">R$ ${produto.preco.toFixed(2)}</p>
     </div>
-    <div class="mx-2 mt-2">
       <h2 class="text-sm sm:text-base font-medium leading-tight">${produto.nome}</h2>
         <a href="produto.html?id=${produto.id}">
      <button class="bg-[#0d2f42] text-gray-200 text-xs sm:text-sm md:text-base px-2 sm:py-2 sm:px-2 py-1 mt-4 rounded hover:bg-[#123f59] w-full">
@@ -134,14 +175,13 @@ function gerarEstrelas(rating) {
   let estrelas = "";
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
-      estrelas += `<i class="fa fa-star text-yellow-500  md:text-base text-sm my-2"></i>`;
+      estrelas += `<i class="fa fa-star text-yellow-500  md:text-base text-sm my-2 lg:text-lg"></i>`;
     } else {
-      estrelas += `<i class="fa fa-star text-gray-300 md:text-base text-sm my-2"></i>`;
+      estrelas += `<i class="fa fa-star text-gray-300 md:text-base text-sm my-2 lg:text-lg"></i>`;
     }
   }
   return estrelas;
 }
-
 
 
 
